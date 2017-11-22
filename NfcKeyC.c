f79da94fa6fd03ca3119324ef72a3d4036f5cde2
Jack <jackfagner@users.noreply.github.com>
2017-11-22 21:46:19 +0100

    Update NfcKeyC.c

--- NfcKeyC.c
+++ NfcKeyC.c
@@ -78,9 +78,8 @@ uint32_t getkey(uint8_t* uid)
 	//Rotate
 	uint8_t r = (uid[1] + uid[3] + uid[5] + uid[7]) & 7; //Rotation offset
 	uint8_t ru[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; //Rotated UID
-	for (i = 0; i < 8; i++) {
-		if (i < 7) //Algorithm assume 8 byte UID, so make sure last one is considered 0x00
-			ru[(i + r) & 7] = uid[i];
+	for (i = 0; i < 7; i++) {
+		ru[(i + r) & 7] = uid[i];
 	}
 	
 	//Transform
@@ -101,9 +100,8 @@ uint16_t getpack(uint8_t* uid)
 	//Rotate
 	uint8_t r = (uid[2] + uid[5] + uid[7]) & 7; //Rotation offset
 	uint8_t ru[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; //Rotated UID
-	for (i = 0; i < 8; i++) {
-		if (i < 7) //Algorithm assume 8 byte UID, so make sure last one is considered 0x00
-			ru[(i + r) & 7] = uid[i];
+	for (i = 0; i < 7; i++) {
+		ru[(i + r) & 7] = uid[i];
 	}
 	
 	//Transform
