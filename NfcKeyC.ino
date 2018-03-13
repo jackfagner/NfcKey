45b6511440d618031779e2221c32d5d129ad449c
Jack <jackfagner@users.noreply.github.com>
2018-03-13 22:13:25 +0100

    Added AGPL header

--- NfcKeyC.ino
+++ NfcKeyC.ino
@@ -1,3 +1,21 @@
+/*
+    Copyright (C) 2018  github.com/Sinitax
+
+    This file is part of NfcKey.
+
+    NfcKey is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NfcKey is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with NfcKey.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <SPI.h>
 #include <MFRC522.h>
 
