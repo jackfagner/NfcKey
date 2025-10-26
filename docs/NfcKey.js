95a3eee837ae5729b8b10c2939e5d5e954859e6a
Jack <jackfagner@users.noreply.github.com>
2025-10-26 16:20:52 +0100

    Add files via upload

---
+++ docs/NfcKey.js
@@ -0,0 +1,135 @@
+/*
+    Copyright (C) 2017  github.com/jackfagner
+
+    This file is part of NfcKey.
+
+    NfcKey is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    NfcKey is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with NfcKey.  If not, see <http://www.gnu.org/licenses/>.
+*/
+var NfcKey = function () { };
+
+NfcKey.prototype = (function () {
+    var intConst = [
+        0x6D835AFC, 0x7D15CD97, 0x0942B409, 0x32F9C923, 0xA811FB02, 0x64F121E8,
+        0xD1CC8B4E, 0xE8873E6F, 0x61399BBB, 0xF1B91926, 0xAC661520, 0xA21A31C9,
+        0xD424808D, 0xFE118E07, 0xD18E728D, 0xABAC9E17, 0x18066433, 0x00E18E79,
+        0x65A77305, 0x5AE9E297, 0x11FC628C, 0x7BB3431F, 0x942A8308, 0xB2F8FD20,
+        0x5728B869, 0x30726D5A
+    ];
+
+    var transform = function (rotUid) {
+        var intPos = 0, tmp1, tmp2;
+        var v1 = ((((rotUid[3] << 24) >>> 0) | ((rotUid[2] << 16) >>> 0) | ((rotUid[1] << 8) >>> 0) | rotUid[0]) >>> 0) + intConst[intPos++];
+        var v2 = ((((rotUid[7] << 24) >>> 0) | ((rotUid[6] << 16) >>> 0) | ((rotUid[5] << 8) >>> 0) | rotUid[4]) >>> 0) + intConst[intPos++];
+
+        for (var i = 0; i < 12; i += 2) {
+            tmp1 = rotateLeft((v1 ^ v2) >>> 0, v2 & 0x1F) + intConst[intPos++];
+            tmp2 = rotateLeft((v2 ^ tmp1) >>> 0, tmp1 & 0x1F) + intConst[intPos++];
+            v1 = rotateLeft((tmp1 ^ tmp2) >>> 0, tmp2 & 0x1F) + intConst[intPos++];
+            v2 = rotateLeft((tmp2 ^ v1) >>> 0, v1 & 0x1F) + intConst[intPos++];
+        }
+        
+        var r = new Uint8Array(8);
+        r[0] = v1 & 0xFF;
+        r[1] = (v1 >>> 8) & 0xFF;
+        r[2] = (v1 >>> 16) & 0xFF;
+        r[3] = (v1 >> 24) & 0xFF;
+        r[4] = v2 & 0xFF;
+        r[5] = (v2 >>> 8) & 0xFF;
+        r[6] = (v2 >>> 16) & 0xFF;
+        r[7] = (v2 >>> 24) & 0xFF;
+        return r;
+    };
+
+    var rotateLeft = function (x, n) {
+        return (((x << n) >>> 0) | (x >>> (32 - n))) >>> 0;
+    };
+
+    var parseHexUid = function (hexUid) {
+        var r = new Uint8Array(8);
+        if (!isValidHexUid(hexUid))
+            return r;
+        for (var i = 0; i < hexUid.length / 2; i++)
+            r[i] = parseInt(hexUid.substr(i * 2, 2), 16);
+        return r;
+    };
+
+    var isValidHexUid = function (hexUid) {
+        return typeof hexUid === "string" && hexUid.length === 14;
+    };
+
+    var swap16 = function (val) {
+        return ((((val & 0xFF) << 8) >>> 0)
+            | ((val >>> 8) & 0xFF)) >>> 0;
+    };
+
+    var swap32 = function (val) {
+        return ((((val & 0xFF) << 24) >>> 0)
+            | (((val & 0xFF00) << 8) >>> 0)
+            | ((val >>> 8) & 0xFF00)
+            | ((val >>> 24) & 0xFF)) >>> 0;
+    };
+
+    var tohex = function (b, l) {
+        var h = b.toString(16).toUpperCase();
+        while (l && h.length < l / 4)
+            h = '0' + h;
+        return h;
+    };
+
+    return {
+
+        constructor: NfcKey,
+
+        getKey: function (uid) {
+            if (!isValidHexUid(uid))
+                return;
+            var i;
+            var uid8 = parseHexUid(uid);
+            var rotUid = new Uint8Array(8);
+            var rotation = ((uid8[1] + uid8[3] + uid8[5]) & 7) >>> 0;
+            for (i = 0; i < 7; i++)
+                rotUid[((i + rotation) & 7) >>> 0] = uid8[i];
+
+            var transfUid = transform(rotUid);
+
+            var intKey = 0;
+            var offset = (transfUid[0] + transfUid[2] + transfUid[4] + transfUid[6]) & 3;
+            for (i = 0; i < 4; i++)
+                intKey = transfUid[i + offset] + ((intKey << 8) >>> 0);
+
+            return tohex(swap32(intKey), 32);
+        },
+
+        getPack: function (uid) {
+            if (!isValidHexUid(uid))
+                return;
+            var i;
+            var uid8 = parseHexUid(uid);
+            var rotUid = new Uint8Array(8);
+            var rotation = ((uid8[2] + uid8[5]) & 7) >>> 0;
+            for (i = 0; i < 7; i++)
+                rotUid[((i + rotation) & 7) >>> 0] = uid8[i];
+
+            var transfUid = transform(rotUid);
+
+            var intPack = 0;
+            for (i = 0; i < 8; i++)
+                intPack += transfUid[i] * 13;
+
+            var res = ((intPack ^ 0x5555) >>> 0) & 0xFFFF;
+            return tohex(swap16(res), 16);
+        }
+
+    };
+})();
---
+++ docs/script.js
@@ -0,0 +1,96 @@
+﻿
+var gN = function (name) { return Array.prototype.slice.call(document.getElementsByTagName(name)); };
+var gI = function (id) { return document.getElementById(id); };
+var lastUid = '';
+var nfcKey = null;
+
+document.addEventListener('DOMContentLoaded', function () {
+    nfcKey = new NfcKey();
+
+    gI('uid').value = '';
+    gI('page0').value = '';
+    gI('page1').value = '';
+
+    gN('input').forEach(function (el, i) {
+        el.addEventListener('keyup', fieldChange);
+        el.addEventListener('change', fieldChange);
+    });
+
+});
+
+function fieldChange(e) {
+    var fieldId = e.target.id;
+    if (fieldId === 'uid') {
+        var uid = gI('uid').value.replace(/ /gi, '');
+        if (isValidUid(uid)) {
+            gI('page0').value = (uid.substr(0, 6) + calcChecksum(uid.substr(0, 6))).toUpperCase();
+            gI('page1').value = uid.substr(6).toUpperCase();
+        }
+        else {
+            gI('page0').value = '';
+            gI('page1').value = '';
+        }
+    } else {
+        var page0 = gI('page0').value.replace(/ /gi, '');
+        var page1 = gI('page1').value.replace(/ /gi, '');
+        if (page0.length === 8 && page1.length === 8) {
+            gI('uid').value = (page0.substr(0, 6) + page1).toUpperCase();
+        }
+        else {
+            gI('uid').value = '';
+        }
+    }
+
+    var newUid = gI('uid').value.replace(/ /gi, '').toUpperCase();
+    if (newUid !== lastUid) {
+        lastUid = newUid;
+        calcKeyPack(newUid);
+    }
+}
+
+function isValidUid(uid) {
+	if (!uid)
+		return false;
+	return new RegExp(/[A-F0-9]{14}/i).test(uid);
+}
+
+function calcChecksum(uid1) {
+    if (uid1.length !== 6)
+        return 'XX';
+    var b1 = parseInt(uid1.substr(0, 2), 16);
+    var b2 = parseInt(uid1.substr(2, 2), 16);
+    var b3 = parseInt(uid1.substr(4, 2), 16);
+    var cs = (b1 ^ b2 ^ b3 ^ 0x88).toString(16).toUpperCase();
+    if (cs.length < 2)
+      cs = '0' + cs;
+    return cs;
+}
+
+function calcKeyPack(uid) {
+    if (!isValidUid(uid)) {
+        showData('', '', '');
+        return;
+    }
+    var key = nfcKey.getKey(uid);
+    var pack = nfcKey.getPack(uid);
+    showData(uid, key, pack);
+}
+
+function showData(uid, key, pack) {
+    document.querySelector('p.uid span').innerText = addSpace(uid);
+    document.querySelector('p.key span').innerText = addSpace(key);
+    document.querySelector('p.pack span').innerText = addSpace(pack);
+    var disp = uid.length === 14 ? 'block' : '';
+    document.querySelector('div.r').style.display = disp;
+    document.querySelector('div.d').style.display = disp;
+}
+
+function addSpace(hex) {
+	if (typeof hex !== 'string' || hex.length === 0)
+		return '';
+    var out = hex.substr(0, 2);
+    for (var i = 2; i < hex.length; i += 2) {
+        out += ' ' + hex.substr(i, 2);
+    }
+    return out;
+}
